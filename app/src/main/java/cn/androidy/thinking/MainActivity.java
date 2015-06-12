19bef764703d918d62f304ca262b3a0c86491950
mengweiping <mengweiping@qufenqi.com>
2015-06-12 15:29:50 +0800

    add floatingActionButton

--- app/src/main/java/cn/androidy/thinking/MainActivity.java
+++ app/src/main/java/cn/androidy/thinking/MainActivity.java
@@ -1,28 +1,46 @@
 package cn.androidy.thinking;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
     private ActionBar mActionBar;
+    private FloatingActionButton mFloatingActionButton;
+
+    private ArrayList<Integer> mFloatingActionButtonImageResIdList;
+    private int mCurrentColorIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mActionBar = getSupportActionBar();
-//        mActionBar.setDisplayHomeAsUpEnabled(true);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mNavigationView = (NavigationView) findViewById(R.id.navigationView);
+        mNavigationView.setNavigationItemSelectedListener(this);
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        mFloatingActionButton.setOnClickListener(this);
+        mFloatingActionButtonImageResIdList = new ArrayList<Integer>();
+        mFloatingActionButtonImageResIdList.add(R.drawable.abc_btn_rating_star_off_mtrl_alpha);//已添加到收藏
+        mFloatingActionButtonImageResIdList.add(R.drawable.abc_btn_rating_star_on_mtrl_alpha);//未添加到收藏
+        mFloatingActionButtonImageResIdList.add(R.drawable.ic_favorite_border_white_48dp);
+        mFloatingActionButtonImageResIdList.add(R.drawable.ic_favorite_white_48dp);
+        mFloatingActionButtonImageResIdList.add(R.drawable.ic_explore_white_48dp);
+        mFloatingActionButtonImageResIdList.add(R.drawable.ic_supervisor_account_white_48dp);
+        mFloatingActionButton.setImageResource(mFloatingActionButtonImageResIdList.get(mCurrentColorIndex));
+        mCurrentColorIndex = (mCurrentColorIndex + 1) % mFloatingActionButtonImageResIdList.size();
     }
 
     @Override
@@ -40,16 +58,37 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_drawer_menu) {
             mDrawerLayout.openDrawer(mNavigationView);
-            return true;
-        } else if (id == R.id.home) {
-            finish();
             return true;
         } else if (id == R.id.action_search) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        if (id == R.id.action_about) {
+            mDrawerLayout.closeDrawers();
+            return true;
+        } else if (id == R.id.action_settings) {
+            mDrawerLayout.closeDrawers();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.floatingActionButton:
+                mFloatingActionButton.setImageResource(mFloatingActionButtonImageResIdList.get(mCurrentColorIndex));
+                mCurrentColorIndex = (mCurrentColorIndex + 1) % mFloatingActionButtonImageResIdList.size();
+                break;
+        }
     }
 }
